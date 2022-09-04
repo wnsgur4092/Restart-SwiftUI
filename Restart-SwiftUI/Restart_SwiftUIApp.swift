b1bf97cd12212861f66824e1a7eefa47be99817b
Jun <wnsgur4092@gmail.com>
2022-09-04 23:13:31 +1000

    Initial Commit

---
+++ Restart-SwiftUI/Restart_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_SwiftUIApp.swift
+//  Restart-SwiftUI
+//
+//  Created by JunHyuk Lim on 4/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct Restart_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
