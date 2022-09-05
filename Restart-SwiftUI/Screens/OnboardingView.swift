f3fa62e33a4eefbd04c7040cdf93eb6ae69707f0
Jun <wnsgur4092@gmail.com>
2022-09-05 15:08:02 +1000

    Final ( + Sound & Haptic)

--- Restart-SwiftUI/Screens/OnboardingView.swift
+++ Restart-SwiftUI/Screens/OnboardingView.swift
@@ -16,6 +16,8 @@ struct OnboardingView: View {
     @State private var imageOffset : CGSize = .zero
     @State private var indicatorOpacity : Double = 1.0
     @State private var textTitle : String = "Share."
+    
+    let hapticFeedback = UINotificationFeedbackGenerator()
 
     var body: some View {
 
@@ -158,9 +160,12 @@ struct OnboardingView: View {
                                 .onEnded({ _ in
                                     withAnimation(Animation.easeOut(duration: 0.4)){
                                         if buttonOffset > buttonWidth / 2 {
+                                            hapticFeedback.notificationOccurred(.success)
+                                            playSound(sound: "chimeup", type: "mp3")
                                             buttonOffset = buttonWidth - 80
                                             isOnboardingViewActive = false
                                         } else {
+                                            hapticFeedback.notificationOccurred(.warning)
                                             buttonOffset = 0
                                         }
                                     }
@@ -183,6 +188,7 @@ struct OnboardingView: View {
         .onAppear {
             isAnimating = true
         }
+        .preferredColorScheme(.dark)
 
     }
 }
