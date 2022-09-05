accec30752e1492935b9c4f7608dd19ea2cba6dc
Jun <wnsgur4092@gmail.com>
2022-09-05 13:46:28 +1000

    Completed Button Offset (Dragging)

--- Restart-SwiftUI/Screens/OnboardingView.swift
+++ Restart-SwiftUI/Screens/OnboardingView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive : Bool = true
     
+    @State private var buttonWidth : Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset :  CGFloat = 0
+    
     var body: some View {
         ZStack {
             Color("ColorBlue")
@@ -65,15 +68,17 @@ struct OnboardingView: View {
                         .fontWeight(.bold)
                         .foregroundColor(.white)
                         .offset(x: 20)
+                    
+                    
+                    //3.Capsule (Dynamic Width)
                     HStack{
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
+                        
                         
                         Spacer()
                     }
-                    
-                    //3.Capsule (Dynamic Width)
                     
                     //4.Circle (Draggable)
                     HStack {
@@ -89,15 +94,31 @@ struct OnboardingView: View {
                         }
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    if buttonOffset > buttonWidth / 2 {
+                                        buttonOffset = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    } else {
+                                        buttonOffset = 0
+                                    }
+                                })
+                        ) // : Gesture
                         
                         Spacer()
                     } //: HStack
                 } // : Footer
-                .frame(height: 80, alignment: .center)
+                .frame(width:buttonWidth, height: 80, alignment: .center)
                 .padding()
+                
+                
                 
             } // : VStack
         } // : ZStack
