85eec4f367a50cbfe85553b03f718ed42ad9d87a
Jun <wnsgur4092@gmail.com>
2022-09-05 11:44:33 +1000

    Completed UI Design

--- Restart-SwiftUI/Screens/HomeView.swift
+++ Restart-SwiftUI/Screens/HomeView.swift
@@ -15,18 +15,42 @@ struct HomeView: View {
             //MARK: - Header
             Spacer()
             
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
+            }
+            
             
             //MARK: - Center
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
             
             //MARK: - Footer
+            Spacer()
             
             Button {
                 isOnboardingViewActive = true
             } label: {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                
                 Text("Restart")
-            }
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            } // : Button
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
 
-        }
+        } // : Vstack
     }
 }
 
--- Restart-SwiftUI/Screens/OnboardingView.swift
+++ Restart-SwiftUI/Screens/OnboardingView.swift
@@ -39,14 +39,7 @@ struct OnboardingView: View {
                 } // : Header
                 //MARK: - Center
                 ZStack {
-                    ZStack{
-                        Circle()
-                            .stroke(.white.opacity(0.2), lineWidth: 40)
-                            .frame(width: 260, height: 260, alignment: .center)
-                        Circle()
-                            .stroke(.white.opacity(0.2), lineWidth: 80)
-                            .frame(width: 260, height: 260, alignment: .center)
-                    } //: ZStack
+                    CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
                     
                     Image("character-1")
                         .resizable()
